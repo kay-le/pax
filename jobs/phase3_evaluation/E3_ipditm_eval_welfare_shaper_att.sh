14a2948b1349330e4a20f30ab3f69d0e38c87dc6
Chenqi Li <lichenqi05@gmail.com>
2026-04-25 23:51:10 -0600

    job: eval

--- jobs/phase3_evaluation/E3_ipditm_eval_welfare_shaper_att.sh
+++ jobs/phase3_evaluation/E3_ipditm_eval_welfare_shaper_att.sh
@@ -29,7 +29,7 @@ if [ -z "$SLURM_JOB_ID" ]; then
             sbatch \
                 --account=def-jtyao_gpu \
                 --job-name=E3_ipditm_eval_s${SEED} \
-                --gpus=nvidia_h100_80gb_hbm3_1g.10gb:1 \
+                --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1 \
                 --cpus-per-task=2 \
                 --mem=10G \
                 --time=0:20:00 \
